cce10de57e29f2f55d02430680867e98588aa7b4
WinSukon <jakrinsukon@gmail.com>
2023-09-07 18:15:18 +0700

    assignment 5 : Change from using hover in tailwind to add/remove classList

--- src/components/InteractiveCard.tsx
+++ src/components/InteractiveCard.tsx
@@ -5,12 +5,31 @@ const ClientCompCard = ({children}:{children:React.ReactNode}) => {
 
     function onCardClicked(){
         alert('card clicked')
+        
+    }
+
+    function onMouseAction(e:React.SyntheticEvent){
+        if(e.type==='mouseover'){
+            e.currentTarget.classList.remove('shadow-lg')
+            e.currentTarget.classList.add('shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)]')
+
+            e.currentTarget.classList.remove('bg-white')
+            e.currentTarget.classList.add('bg-neutral-200')
+        }
+        else{
+            e.currentTarget.classList.remove('shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)]')
+            e.currentTarget.classList.add('shadow-lg')
+
+            e.currentTarget.classList.remove('bg-neutral-200')
+            e.currentTarget.classList.add('bg-white')
+        }
     }
 
     return ( 
-       <div className="w-[250px] h-[300px] rounded-lg shadow-lg  m-8  
-       transition ease-in-out delay-150 hover:scale-105 hover:shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)]"
-       onClick={()=>onCardClicked()}>
+       <div className="w-[250px] h-[300px] rounded-lg m-8 shadow-lg bg-white"
+       onClick={()=>onCardClicked()}
+       onMouseOver={(e)=>onMouseAction(e)}
+       onMouseOut={(e)=>onMouseAction(e)}>
         {children}
        </div> 
     );
