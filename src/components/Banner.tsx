c42b1afffbbcffc08a3902e37ff196512a803ab7
WinSukon <jakrinsukon@gmail.com>
2023-09-12 12:43:56 +0700

    assignment 6 : banner img can now change

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,25 +1,19 @@
+'use client'
+
+import { useState } from "react";
+
 import Image from "next/image";
 import style from "./Banner.module.css"
 const banner = ({name}:{name:string}) => {
+
+    const [index,setIndex] = useState(0);
+    const imgSrc=["/img/CovidVaccineSurvey_Header.jpg","/img/covid-19-vaccination.webp","/img/vaccine-vial-scaled.jpg","/img/vaccine-banner.jpg"];
+
     return ( 
         <div>
+            <div className="block h-[60vh] w-[100vw] p-[5px] m-[0px] relative" onClick={()=>(setIndex(index+1))}>
 
-            {/* <div className={style.banner}>
-
-                <Image src={"/img/CovidVaccineSurvey_Header.jpg"} 
-                alt='cover' 
-                fill={true} 
-                objectFit="cover"></Image>
-                <div className={style.bannerText}>
-                    <h1 className="text-6xl font-medium">Our Services</h1>
-                    <p className="text-2xl font-serif">{name}</p>
-                </div>
-            </div> */}
-
-
-            <div className="block h-[60vh] w-[100vw] p-[5px] m-[0px] relative">
-
-                <Image src={"/img/CovidVaccineSurvey_Header.jpg"} 
+                <Image src={imgSrc[index%4]} 
                 alt='cover' 
                 fill={true} 
                 objectFit="cover"></Image>
