4f2a3ccd146b91618863c2a0910f7e5a49b4fb7e
WinSukon <jakrinsukon@gmail.com>
2023-10-02 23:24:35 +0700

    assignment 8 : PromoteCard fixed and added more style

--- src/components/PromoteCard.tsx
+++ src/components/PromoteCard.tsx
@@ -4,17 +4,18 @@ import VideoPlayer from "./VideoPlayer";
 
 const PromoteCard = () => {
     const [playing,setPlay] = useState(true)
+
     return ( 
-        <div className='flex flex-row justify-around'>
-           <div className="flex m-5">
-                <VideoPlayer vdoSrc="./video/getvaccine.mp4" isPlaying={playing} ></VideoPlayer>
+        <div className='flex flex-row w-[60vw] shadow-lg p-2 justify-around bg-slate-300 m-5 rounded-md'>
+        
+            <VideoPlayer vdoSrc="./video/getvaccine.mp4" isPlaying={playing} ></VideoPlayer>
 
-           </div>
+      
 
-    
+
             <div className="flex flex-col  justify-between">
-                <div className="text-lg items-center">video</div>
-                <button className='m-5 p-3 border-solid border-2 border-sky-500' onClick={()=>{setPlay(!playing)}}>pause/play</button>
+                <div className="text-lg items-center m-5">Get your vaccine today</div>
+                <button className='m-5 p-3  bg-sky-400 rounded-xl block hover:bg-sky-500' onClick={()=>{setPlay(!playing)}}>pause/play</button>
             </div>
         </div>
      );
--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -1,13 +1,12 @@
-import {useRef} from 'react'
+import {useRef,useEffect} from 'react'
 
 const VideoPlayer = ({vdoSrc,isPlaying}:{vdoSrc:string,isPlaying:boolean}) => {
     const vdoRef = useRef<HTMLVideoElement>(null)
-    if(isPlaying){
-        vdoRef.current?.play();
-    }
-    else{
-        vdoRef.current?.pause();
-    }
+
+
+    useEffect(()=>{
+        isPlaying ?  vdoRef.current?.play(): vdoRef.current?.pause();
+    },[isPlaying])
 
     return ( 
         <video className='w-[50%]' src={vdoSrc} ref={vdoRef} muted loop controls></video>
