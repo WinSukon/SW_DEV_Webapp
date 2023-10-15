ddcf3451f3e7c55ea287e1789bdbad6ea78053be
WinSukon <jakrinsukon@gmail.com>
2023-10-15 13:40:47 +0700

    fix : trying to fix git

---
+++ src/lib/getHospitals.tsx
@@ -0,0 +1,13 @@
+
+// export default async function getHospital() {
+//     await fetch()
+// } ;
+const gethos = () => {
+    return ( 
+        <div>
+            hahaha
+        </div>
+     );
+}
+ 
+export default gethos;
