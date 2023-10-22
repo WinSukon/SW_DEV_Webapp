6368f67432fd00dc4e5f665205272a84d280bb12
WinSukon <jakrinsukon@gmail.com>
2023-10-22 16:20:17 +0700

    assignment 11 :  fix select hos in form so that it show what is selected

--- Frontend/src/components/Form.tsx
+++ Frontend/src/components/Form.tsx
@@ -1,12 +1,20 @@
 'use client'
 
-import { Select , MenuItem ,TextField } from "@mui/material";
+import { Select , MenuItem , SelectChangeEvent } from "@mui/material";
 import { DatePicker } from "@mui/x-date-pickers";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import MyText from "./MyText";
+import {useState} from 'react';
 
 const Form = () => {
+
+    const [hos,setHos] = useState('')
+
+    const handleChange=(event: SelectChangeEvent)=>{
+        setHos(event.target.value);
+    }
+
     return (  
         <form >
             <div className="flex flex-row m-3">
@@ -22,10 +30,10 @@ const Form = () => {
                 
                     <div className="text-base p-3">Select Hospital</div>
                     <div className="p-3 mb-4">
-                        <Select variant="standard" label="choose hospital" className="w-[280px]">
-                            <MenuItem>Chulalongkorn Hospital</MenuItem>
-                            <MenuItem>Rajavithi Hospital</MenuItem>
-                            <MenuItem>Thammasat University Hospital</MenuItem>
+                        <Select variant="standard" label="choose hospital" className="w-[280px]" value={hos} onChange={handleChange}>
+                            <MenuItem value={'Chulalongkorn Hospital'}>Chulalongkorn Hospital</MenuItem>
+                            <MenuItem value={'Rajavithi Hospital'}>Rajavithi Hospital</MenuItem>
+                            <MenuItem value={'Thammasat University Hospital'}>Thammasat University Hospital</MenuItem>
 
                         </Select>
                     </div>
@@ -44,7 +52,7 @@ const Form = () => {
 
             </div>
             <div className="left-[46%] absolute m-0">
-            <button className="rounded-md bg-sky-600 text-white px-3 py-2  shadow-sm hover:bg-indigo-600">Confirm</button>
+                <button className="rounded-md bg-sky-600 text-white px-3 py-2  shadow-sm hover:bg-indigo-600">Confirm</button>
 
             </div>
 
