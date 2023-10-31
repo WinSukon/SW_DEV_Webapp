afa00f1d8690d99cebe9ac6e9dbe09b87476ee08
WinSukon <jakrinsukon@gmail.com>
2023-10-31 09:13:04 +0700

    assignment 12 : make hospitaCatalog responsive

--- Frontend/src/components/hospitalCatalog.tsx
+++ Frontend/src/components/hospitalCatalog.tsx
@@ -5,21 +5,17 @@ import Link from 'next/link';
 const hospitalCatalog = async ({hosJson}:{hosJson:Object}) => {
     const hosJsonReady = await hosJson;
     return ( 
-        <div className="flex flex-col">
-            <div className="flex flex-row ">
-
-                { hosJsonReady.data.map((hos:Object)=>(
-                    <Link href={`/hospital/${hos.id}`}>
-                        <Card title={hos.name}
-                        imgSrc={hos.picture}
-                        />
-                    </Link>
+        <div className="flex flex-row flex-wrap p-[10px] justify-around">
+            { hosJsonReady.data.map((hos:Object)=>(
+                <Link href={`/hospital/${hos.id}`} className="w-[100%] sm:w-[50%] md:w-[30%] lg:w-[25%] p-2 sm:p-4 md:p-4 lg:p-8">
+                    <Card title={hos.name}
+                    imgSrc={hos.picture}
+                    />
+                </Link>
             
                 ))}
-                
-
-            </div>
-        </div> 
+        </div>
+        
     );
 }
  
